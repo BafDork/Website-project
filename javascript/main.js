783c1774c352ecd850834c9146f430679dd013d2
BafDork <eugrne@gmail.com>
2022-03-08 01:40:23 +0300

    Proj_bas normal code

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,132 @@
-let scene, camera, renderer, composer, flash;
+let scene, camera, renderer, composer, flash, mouse;
+let tex_loader, obj_loader, mtl_loader;
+let starGeo, stars;
 let cloudParticles = [];
 
 function init()
  {
+	initScene();
+	initCamera();
+	initRenderer();
+	initPostprocessing();
+
+	initLoaders();
+	initEventListeners();
+	initControls();
+
+	let axesHelper = new THREE.AxesHelper(500);
+	scene.add(axesHelper);
+	scene.fog = new THREE.FogExp2(0x03544e, 0.001);
+	renderer.setClearColor(scene.fog.color);
+
+	initLights();
+	loadModel();
+	initStars();
+	initClouds();
+
+	document.body.appendChild(renderer.domElement);
+
+	render();
+}
+
+function render()
+{
+	animateClouds();
+	animateStars();
+
+	composer.render(0.1);
+	requestAnimationFrame(render);
+}
+
+function animateClouds()
+{
+	cloudParticles.forEach(p => 
+	{
+		p.rotation.z -= 0.001;
+	});
+
+	if (Math.random() > 0.95 || flash.power > 200)
+	{
+		if (flash.power < 100) flash.position.set(
+			Math.random() * 400,
+			300 + Math.random() * 200,
+			100
+			);
+		flash.power = 50 + Math.random() * 500;
+	}
+}
+
+function animateStars()
+{
+	starGeo.vertices.forEach(p => 
+	{
+		p.velocity += p.acceleration
+		p.y -= p.velocity;
+		
+		if (p.y < -200)
+		{
+			p.y = 200;
+			p.velocity = 0;
+		}
+	});
+	starGeo.verticesNeedUpdate = true;
+	stars.rotation.y += 0.002;
+}
+
+function initScene() 
+{
 	scene = new THREE.Scene();
+}
+
+function initCamera() 
+{
 	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 	camera.position.z = 1;
 	camera.rotation.x = 1.16;
 	camera.rotation.y = -0.12;
 	camera.rotation.z = 0.27;
+}
 
+function initRenderer()
+{
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function initPostprocessing()
+{
+	const bloomEffect = new POSTPROCESSING.BloomEffect(
+	{
+		blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+		kernelSize: POSTPROCESSING.KernelSize.SMALL,
+		useLuminanceFilter: true,
+		luminanceThreshold: 0.3,
+		luminanceSmoothing: 0.75
+	});
+	bloomEffect.blendMode.opacity.value = 1.5;
+
+	let effectPass = new POSTPROCESSING.EffectPass(
+		camera,
+		bloomEffect
+	);
+	effectPass.renderToScreen = true;
+
+	composer = new POSTPROCESSING.EffectComposer(renderer);
+	composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
+	composer.addPass(effectPass);
+}
+
+function initLights()
+{
 	let ambient = new THREE.AmbientLight(0x555555);
 	scene.add(ambient);
 
 	let directionalLight = new THREE.DirectionalLight(0xff8c19);
 	directionalLight.position.set(0, 0, 1);
 	scene.add(directionalLight);
+
+	// const helper = new THREE.DirectionalLightHelper(directionalLight);
+	// scene.add(helper);
 
 	let orangeLight = new THREE.PointLight(0xcc6600, 50, 450, 1.7);
 	orangeLight.position.set(200, 300, 100);
@@ -32,24 +143,24 @@ function init()
 	flash = new THREE.PointLight(0x062d89, 30, 500, 1.7);
 	flash.position.set(200, 300, 100);
 	scene.add(flash);
+}
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	scene.fog = new THREE.FogExp2(0x03544e, 0.001);
-	renderer.setClearColor(scene.fog.color);
-	document.body.appendChild(renderer.domElement);
+function initLoaders() 
+{
+	tex_loader = new THREE.TextureLoader();
+	let loading_manager = new THREE.LoadingManager();
+	obj_loader = new THREE.OBJLoader(loading_manager);
+	mtl_loader = new THREE.MTLLoader();
+}
 
-	
-	let LOADING_MANAGER;
-	let OBJ_LOADER;
-	let MTL_Loader;
-	LOADING_MANAGER = new THREE.LoadingManager();
-	OBJ_LOADER = new THREE.OBJLoader(LOADING_MANAGER);
-	MTL_Loader = new THREE.MTLLoader();
-	MTL_Loader.load("data/model/new_obj.mtl", function (materials) {
+function loadModel()
+{
+	mtl_loader.load("data/model/new_obj.mtl", function (materials) 
+	{
 		materials.preload();
-		OBJ_LOADER.setMaterials(materials);
-		OBJ_LOADER.load("data/model/new_obj.obj", function (object) {
+		obj_loader.setMaterials(materials);
+		obj_loader.load("data/model/new_obj.obj", function (object) 
+		{
 			// object.traverse(part => { 
 			// 	if (part.isMesh) 
 			// 	{
@@ -67,14 +178,18 @@ function init()
 			scene.add(object);
 		});
 	});
-	
+}
 
+function initStars()
+{
 	starGeo = new THREE.Geometry();
-	for (let i = 0; i < 6000; i++) {
+	for (let i = 0; i < 6000; i++)
+	{
 		let star = new THREE.Vector3(
-		Math.random() * 600 - 300,
-		Math.random() * 600 - 300,
-		Math.random() * 600 - 300);
+			Math.random() * 600 - 300,
+			Math.random() * 600 - 300,
+			Math.random() * 600 - 300
+			);
 		star.velocity = 0;
 		star.acceleration = 0.02;
 		starGeo.vertices.push(star);
@@ -86,19 +201,23 @@ function init()
 		size: 0.7,
 		map: sprite});
 	*/
-	let starMaterial = new THREE.PointsMaterial({
+	let starMaterial = new THREE.PointsMaterial(
+	{
 		color: 0xaaaaaa,
 		size: 0.3,
-		transparent: true});
+		transparent: true
+	});
 
 	stars = new THREE.Points(starGeo, starMaterial);
 	scene.add(stars);
+}
 
-	let loader = new THREE.TextureLoader();
-	loader.load("data/smoke.png", function(texture) 
+function initClouds()
+{
+	tex_loader.load("data/smoke.png", function(texture) 
 	{
-		cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
-		cloudMaterial = new THREE.MeshLambertMaterial(
+		let cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+		let cloudMaterial = new THREE.MeshLambertMaterial(
 		{
 			map: texture,
 			transparent: true
@@ -110,7 +229,8 @@ function init()
 			cloud.position.set(
 				Math.random() * 800 - 400,
 				500,
-				Math.random() * 500 - 500);
+				Math.random() * 500 - 500
+				);
 			cloud.rotation.x = 1.16;
 			cloud.rotation.y = -0.12;
 			cloud.rotation.z = Math.random() * 2 * Math.PI;
@@ -119,80 +239,33 @@ function init()
 			scene.add(cloud);
 		}
 	});
-
-	loader.load("data/space.jpg", function(texture)
-	{
-		const textureEffect = new POSTPROCESSING.TextureEffect(
-		{
-			blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
-			texture: texture
-		});
-		textureEffect.blendMode.opacity.value = 0.2;
-
-		const bloomEffect = new POSTPROCESSING.BloomEffect(
-		{
-			blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
-			kernelSize: POSTPROCESSING.KernelSize.SMALL,
-			useLuminanceFilter: true,
-			luminanceThreshold: 0.3,
-			luminanceSmoothing: 0.75
-		});
-		bloomEffect.blendMode.opacity.value = 1.5;
-
-		let effectPass = new POSTPROCESSING.EffectPass(
-			camera,
-			bloomEffect,
-			textureEffect
-		);
-		effectPass.renderToScreen = true;
-
-		composer = new POSTPROCESSING.EffectComposer(renderer);
-		composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
-		composer.addPass(effectPass);
-
-		window.addEventListener("resize", onWindowResize, false);
-		render();
-	});
 }
 
-function render()
+function initEventListeners() 
 {
-	cloudParticles.forEach(p => 
-	{
-		p.rotation.z -= 0.001;
-	});
-
-	if (Math.random() > 0.95 || flash.power > 200)
-	{
-		if (flash.power < 100) flash.position.set(
-			Math.random() * 400,
-			300 + Math.random() * 200,
-			100);
-		flash.power = 50 + Math.random() * 500;
-	}
-
-	starGeo.vertices.forEach(p => {
-		p.velocity += p.acceleration
-		p.y -= p.velocity;
-		
-		if (p.y < -200)
-		{
-			p.y = 200;
-			p.velocity = 0;
-		}
-	  });
-	starGeo.verticesNeedUpdate = true;
-	stars.rotation.y += 0.002;
-
-	composer.render(0.1);
-	requestAnimationFrame(render);
+    window.addEventListener('resize', onWindowResize);
+    window.addEventListener('mousemove', onMouseMove);
+    onWindowResize();
 }
 
-function onWindowResize()
+function initControls() 
 {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    mouse = new THREE.Vector2();
+}
+
+function onWindowResize() 
+{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+function onMouseMove(event) 
+{
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
 init();
