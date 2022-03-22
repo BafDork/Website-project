453029c38170852d784e81b15c976819325f3f2f
BafDork <eugrne@gmail.com>
2022-03-22 17:14:22 +0300

    Clouds my ver

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,8 @@
+import {initClouds, animateClouds} from './cloud_sys.js';
+
 let scene, camera, renderer, composer, stats;
-let tex_loader, background;
+let tex_loader, background, flash;
+let clouds1 = [], clouds2 = [], clouds3 = [];
 
 function init()
 {
@@ -17,6 +20,11 @@ function init()
 	
 	initBackground();
 
+	initLights();
+	initClouds(scene, clouds1, 50, 1.2, 1, 10, 0xFFA733);
+	initClouds(scene, clouds2, 40, 0.8, 0.8, 9.9, 0xFF7400);
+	initClouds(scene, clouds3, 30, 0.6, 1, 9.8, 0xE60042);
+
 	removeLoading();
 	render();
 }
@@ -29,7 +37,7 @@ function initScene()
 function initCamera() 
 {
 	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.set(0, 0, 2);
+	camera.position.set(0, 0, 11);
 }
 
 function initRenderer()
@@ -113,6 +121,34 @@ function initBackground()
 	});
 }
 
+function initLights()
+{
+	let ambient = new THREE.AmbientLight(0xFFFFFF);
+	scene.add(ambient);
+
+	const light = new THREE.SpotLight(0xFFFFFF, 2, 12)
+	light.position.set(0, 0, 12);
+
+	scene.add(light);
+	scene.add(light.target);
+
+	const helper = new THREE.SpotLightHelper(light);
+	// const helper = new THREE.PointLightHelper(light);
+	// const helper = new THREE.DirectionalLightHelper(directionalLight);
+	scene.add(helper);
+
+	function updateLight() {
+		// directionalLight.target.updateMatrixWorld();
+	  	light.target.updateMatrixWorld();
+	  	helper.update();
+	}
+	updateLight();
+
+	flash = new THREE.PointLight(0xFFFFFF, 30, 500, 1.7);
+	flash.position.set(200, 300, 100);
+	scene.add(flash);
+}
+
 function removeLoading() 
 {
    document.getElementById('loading').style.display = 'none';
@@ -124,7 +160,11 @@ function render()
 {
 	stats.update();
 
+	animateFlash();
 	animateBackground();
+	animateClouds(clouds1, -0.001);
+	animateClouds(clouds2, 0.0015);
+	animateClouds(clouds3, -0.002);
 
 	requestAnimationFrame(render);
 	composer.render(0.1);
@@ -135,6 +175,19 @@ function animateBackground()
 	if (background) 
 	{
 		background.rotation.y -= 0.0005;
+	}
+}
+
+function animateFlash()
+{
+	if (Math.random() > 0.95 || flash.power > 200)
+	{
+		if (flash.power < 100) flash.position.set(
+			Math.random() * 400,
+			300 + Math.random() * 200,
+			100
+			);
+		flash.power = 50 + Math.random() * 500;
 	}
 }
 
